e2a781825e5a8767d5d2e5f9e65b293f09970a15
Julio <jc-rl2@hotmail.com>
2025-01-05 13:16:06 +1000

    Created database and added some data

---
+++ src/db/migrate.ts
@@ -0,0 +1,16 @@
+import { db } from "./index"
+import { migrate } from "drizzle-orm/neon-http/migrator"
+
+const main = async() => {
+    try {
+        await migrate(db, {
+            migrationsFolder: 'src/db/migrations'
+        })
+        console.log('Migration completed')
+    } catch (error) {
+        console.error('Migration failed: ', error)
+        process.exit(1)
+    }
+}
+
+main()
